14295bd06b3f1e46348f4295d047239d560d680e
Kaartik Tejwani <tejwanikaartik@gmail.com>
2025-07-03 13:55:21 -0700

    working on uploads template

--- app/auth.py
+++ app/auth.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+from app.db import get_db
 
 
 #creates a blueprint(which is a many app focused on specifc things) named auth
