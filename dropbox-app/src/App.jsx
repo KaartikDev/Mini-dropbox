c78f7593afbed27636bc63e2bf40b5177b6aeeb4
Kaartik Tejwani <tejwanikaartik@gmail.com>
2025-08-18 07:07:17 -0700

    Fixed Log Out After Refresh

--- dropbox-app/src/App.jsx
+++ dropbox-app/src/App.jsx
@@ -9,8 +9,25 @@ import ProtectedRoute from './protectedRoute'
 
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import { useAuth } from "react-oidc-context";
+import { useEffect } from "react";
+// Function to check if OIDC parameters are present in the URL
+// This is used to clean up the URL after authentication
+function hasOidcParams(search) {
+  const p = new URLSearchParams(search);
+  return p.has("code") || p.has("state");
+} // add other provivers
 
 function App() {
+
+  const auth = useAuth()
+    useEffect(() => {
+        if (hasOidcParams(window.location.search) && !auth.isLoading) {
+        // Replace the current history entry: same path, no query, keep the hash
+        const cleanUrl = window.location.pathname + (window.location.hash || "");
+        window.history.replaceState({}, "", cleanUrl);
+        }
+    }, []);
+
   return(
 
 
--- dropbox-app/src/pages/Home.jsx
+++ dropbox-app/src/pages/Home.jsx
@@ -4,20 +4,10 @@ import StatusIndicator from '../StatusIndicator';
 
 import { useEffect } from "react";
 import { useAuth } from "react-oidc-context";
-function hasOidcParams(search) {
-  const p = new URLSearchParams(search);
-  return p.has("code") || p.has("state"); // add others if your provider uses them
-}
+
 
 function Home(){
-    const auth = useAuth()
-    useEffect(() => {
-        if (hasOidcParams(window.location.search) && !auth.isLoading && auth.isAuthenticated) {
-        // Replace the current history entry: same path, no query, keep the hash
-        const cleanUrl = window.location.pathname + (window.location.hash || "");
-        window.history.replaceState({}, "", cleanUrl);
-        }
-    }, []);
+    
     
     return(
         <>
